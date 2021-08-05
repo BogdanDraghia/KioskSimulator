cbd5bc7859e227e043d0baa9516af57def96123d
Bogdan Draghia <bogdan.draghia@nortic.es>
2021-08-05 10:44:11 +0000

    updated dockerfile..

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Device from "./components/Device";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Controller Kiosk v1.0</h1>
+      <Device />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Device.js
@@ -0,0 +1,5 @@
+function Device() {
+  return <>test</>;
+}
+
+export default Device;
